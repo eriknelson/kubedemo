942a78f3d6c0a25442bf44ebaed3cf5835a739b0
Erik Nelson <erik@nsk.io>
2017-08-31 16:35:12 -0400

    Finalize cluster_* scripts

--- cluster_reset.sh
+++ cluster_reset.sh
@@ -2,3 +2,4 @@
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ${_dir}/cluster_down.sh
 ${_dir}/cluster_up.sh
+${_dir}/post.sh
--- internal_cluster/_cluster_down.sh
+++ internal_cluster/_cluster_down.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 _host=$(hostname)
-export KUBECONFIG=/vagrant/kube-1-master.conf
 
 if [[ "${_host}" == "kube-1-master.example.com" ]]; then
   echo "Running on master, draining nodes, deleting nodes, and resetting"
+  export KUBECONFIG=/etc/kubernetes/admin.conf
   /usr/bin/kubectl drain kube-2-node.example.com --delete-local-data --force --ignore-daemonsets
   /usr/bin/kubectl delete node kube-2-node.example.com
   sudo /usr/bin/kubeadm reset
