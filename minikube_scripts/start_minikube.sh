bad75595888caafb260724fc8ef3173f1b6bcece
Erik Nelson <erik@nsk.io>
2018-03-07 13:38:56 -0500

    Use ~/bin/minikube and kvm2

--- minikube_scripts/start_minikube.sh
+++ minikube_scripts/start_minikube.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkbin=${_dir}/req-header-mk
-#mkbin=minikube
-$mkbin start --extra-config=apiserver.Authorization.Mode=RBAC --vm-driver=kvm
+mkbin=~/bin/minikube
+$mkbin start --extra-config=apiserver.Authorization.Mode=RBAC --vm-driver=kvm2
