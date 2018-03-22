79e72c2f9bfc5c7e95e328a9d6b65f343e248528
Erik Nelson <erik@nsk.io>
2018-03-22 11:12:58 -0400

    Remove unecessary stuff from init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ${_dir}/start_minikube.sh
-${_dir}/permission_ks.sh
+#${_dir}/permission_ks.sh
 ${_dir}/setup_tiller.sh
-echo "Sleeping for 5s to wait for tiller."
-sleep 5s
-${_dir}/install_service_catalog.sh
