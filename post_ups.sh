efc3a17bdb85092672af2a04b4b5256b98d8a049
Erik Nelson <erik@nsk.io>
2018-05-24 17:54:37 -0400

    Updates

--- post_ups.sh
+++ post_ups.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd /git/src/github.com/kubernetes-incubator/service-catalog
-helm install charts/ups-broker --name ups-broker --namespace ups-broker
+kubectl create -f $_dir/ups-broker.yaml
 popd
