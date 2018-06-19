a95f7eac77c1ed7764871f86a1558639ec81c5ab
Erik Nelson <erik@nsk.io>
2018-06-19 18:50:30 -0400

    Updates

--- render_demosetup_template.sh
+++ render_demosetup_template.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-templ=demo-setup.templ.yaml
-outfile=demo-setup.yaml
+templ=$_dir/templates/demo-setup.templ.yaml
+output_dir=$_dir/.output
+mkdir -p ${output_dir}
+outfile=$output_dir/demo-setup.yaml
 CLIENT_TOKEN_NAME=$(kubectl get secrets -n automation-broker | grep 'automation-broker-client-token-*' | awk '{print $1}')
 BROKER_CA_CERT=$(kubectl get secret -n automation-broker automation-broker-tls -o yaml | grep 'tls\.crt' | awk '{print $2}')
 echo $BROKER_CA_CERT
 echo $CLIENT_TOKEN_NAME
-cat $_dir/$templ | sed "s|%%CLIENT_TOKEN_NAME%%|${CLIENT_TOKEN_NAME}|" > $_dir/$outfile
-cat $_dir/$templ | sed -i "s|%%BROKER_CA_CERT%%|${BROKER_CA_CERT}|" $_dir/$outfile
+cat $templ | sed "s|%%CLIENT_TOKEN_NAME%%|${CLIENT_TOKEN_NAME}|" > $outfile
+cat $templ | sed -i "s|%%BROKER_CA_CERT%%|${BROKER_CA_CERT}|" $outfile
 cat $outfile
--- render_secret_template.sh
+++ render_secret_template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-templ=templates/secret.templ.yaml
+templ=$_dir/templates/secret.templ.yaml
 output_dir=$_dir/.output
 mkdir -p ${output_dir}
 outfile=$output_dir/secret.yaml
@@ -8,7 +8,7 @@ CLIENT_TOKEN_NAME=$(kubectl get secrets -n automation-broker | grep 'automation-
 CA_CERT=$(kubectl get secret -n automation-broker $CLIENT_TOKEN_NAME -o yaml | grep 'ca\.crt:' | awk '{print $2}')
 TOKEN=$(kubectl get secret -n automation-broker $CLIENT_TOKEN_NAME -o yaml | grep 'token:' | awk '{print $2}')
 echo $TOKEN
-cat $_dir/$templ | sed "s/%%CA_CERT%%/${CA_CERT}/" > $outfile
+cat $templ | sed "s/%%CA_CERT%%/${CA_CERT}/" > $outfile
 sed -i "s/%%TOKEN%%/${TOKEN}/" $outfile
 sed -i "s|%%SECRET_NAME%%|${CLIENT_TOKEN_NAME}|" $outfile
 cat $outfile
